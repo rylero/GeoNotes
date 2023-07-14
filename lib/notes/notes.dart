6587a24e74b9cf9314b6553ced30513c65f40a2d
Dave LeRoy <leroy.dave@gmail.com>
2023-07-14 16:03:55 -0700

    Added first attempt at swipe to delete list. Still a lot to do, like dynamic list data and fix some route error (try to move back to map).

--- lib/notes/notes.dart
+++ lib/notes/notes.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:geonotes/shared/bottom_nav_bar.dart';
 
-class NotesScreen extends StatelessWidget {
-  const NotesScreen({super.key});
+class NotesScreen extends StatefulWidget {
+  State createState() => NotesScreenState();
+}
+
+class NotesScreenState extends State<NotesScreen> {
+  final items = List<String>.generate(20, (i) => "Item ${i + 1}");
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Notes"),
+    final title = 'Nearby GeoNotes';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      bottomNavigationBar: BottomNavBar(
-        itemIndex: 1,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        // Create a ListView
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              // Specify the direction to swipe and delete
+              direction: DismissDirection.endToStart,
+              key: Key(item),
+              onDismissed: (direction) {
+                // Removes that item the list on swipwe
+                setState(() {
+                  items.removeAt(index);
+                });
+                // Shows the information on Snackbar
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text("$item dismissed")));
+              },
+              background: Container(color: Colors.red),
+              child: ListTile(title: Text('$item')),
+            );
+          },
+        ),
+        bottomNavigationBar: BottomNavBar(
+          itemIndex: 1,
+        ),
       ),
     );
   }
